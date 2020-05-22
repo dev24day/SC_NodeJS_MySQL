8749a081692538006a90f419f29500a01e868816
박경준 <kjyanus@gmail.com>
2020-05-22 02:36:20 +0000

    Connect nodejs&mysql

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');
+var userinfo = require('../userinfo.js');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : userinfo.user,
+  password : userinfo.password,
+  database : 'opentutorials'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM topic', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+ 
+connection.end();
